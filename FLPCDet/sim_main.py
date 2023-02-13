7c1255ed512590b3d50ceaae82dd21a119148744
Shuai <38368612+bearswang@users.noreply.github.com>
2023-02-14 00:09:34 +0800

    Delete sim_main.py

--- FLPCDet/sim_main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import os
-import subprocess as sp
-
-sp.run(['bash', '-c', 'python carla_pretrain_second.py pretrain'] ) # pretrain
-sp.run(['bash', '-c', 'python carla_main_FLCAV_second.py']) # federated learning
-    
