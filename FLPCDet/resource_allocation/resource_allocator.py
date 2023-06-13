1c7a549c95a691a2f3eb807c657708db21126fb2
Shuai <38368612+bearswang@users.noreply.github.com>
2023-06-13 21:10:24 +0800

    Update resource_allocator.py

--- FLPCDet/resource_allocation/resource_allocator.py
+++ FLPCDet/resource_allocation/resource_allocator.py
@@ -23,7 +23,7 @@ class Resource_Allocator:
         # wireline_budget = 4096 # 4MB
 
     def allocate(self, wireless_budget, wireline_budget):
-        a= self.a 
+        a = self.a 
         b = self.b
         D = self.D
         DNNSIZE = self.DNNSIZE
@@ -34,6 +34,9 @@ class Resource_Allocator:
         y = self.y
         z = self.z
         INTERVAL = self.INTERVAL
+        
+        num_veh = 7
+        num_edge = 2
         
         # variables
         x = cp.Variable(n)
@@ -47,8 +50,8 @@ class Resource_Allocator:
         budget_constraints = [cp.sum(cp.sum(u)) <= wireless_budget, cp.sum(cp.sum(v)) <= wireline_budget]
         x_constraints = [x[i] <= u[0][i]/D[i] for i in range(n)]
         x_constraints += [x[i] <= v[0][i]/D[i] for i in range(n)]
-        r_constraints = [r[i] <= u[1][i]/(7*DNNSIZE[i]) for i in range(n)]
-        t_constraints = [t[i] <= v[2][i]/(2*DNNSIZE[i]) for i in range(n)]
+        r_constraints = [r[i] <= u[1][i]/(num_veh*DNNSIZE[i]) for i in range(n)]
+        t_constraints = [t[i] <= v[2][i]/(num_edge*DNNSIZE[i]) for i in range(n)]
         rt_constraints = [r[i] == INTERVAL * t[i] for i in range(n)]
         slack_constraints = [slack[i] >= a[i] * cp.power(alpha[i] * x[i] \
                 + beta[i] * (y[i] + z[i]) * (1 - cp.inv_pos(t[i]+1)), -b[i]) for i in range(n)]
